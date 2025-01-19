1ae65de23d7e4c92c9eebb04c7652aeef971a0ff
ibrahem7495 <52601505+ibrahem7495@users.noreply.github.com>
2025-01-20 00:37:15 +0200

    Updated Angular project with latest changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/components/layout/layout.component';
 import { LoginComponent } from './layout/components/login/login.component';
 import { CreateAccountComponent } from './layout/components/create-account/create-account.component';
-import { HomeComponent } from './layout/components/home/home.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/components/footer/footer.component';
-import { HomeComponent } from './layout/components/home/home.component';
+import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './layout/components/header/header.component';
 import { LayoutComponent } from './layout/components/layout/layout.component';
 import { LoginComponent } from './layout/components/login/login.component';
